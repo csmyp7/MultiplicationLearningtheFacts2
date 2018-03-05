ec3d5839a420504acddbc04921b3933411a09910
McCullough <csmyp7@mail.umkc.edu>
2018-03-04 21:00:40 -0600

    new commit

---
+++ app/src/main/java/com/multiplication/mccullough/multiplicationlearningthefacts/MainActivity.java
@@ -0,0 +1,25 @@
+package com.multiplication.mccullough.multiplicationlearningthefacts;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+import static java.lang.String.valueOf;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/multiplication/mccullough/multiplicationlearningthefacts/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.multiplication.mccullough.multiplicationlearningthefacts;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread t = new Thread(){
+            public void run() {
+                try {
+                    sleep(3000);
+
+                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        t.start();
+    }
+}
